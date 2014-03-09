d4f59e117417cf2bdee2a9236ab19b8615050f88
Masato OSHIMA <qqw75pn9@gmail.com>
2014-03-09 19:18:48 +0900

    Implemented to output json from csv file

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,15 +2,15 @@ module.exports = function(grunt) {
 
 	grunt.initConfig({
 		watch: {
-			files: ['lib/*.js', 'spec/*.js'],
+			files: ['lib/*.js', 'spec/*.js', 'Gruntfile.js'],
 			tasks: ['jshint']
 		},
 		jshint: {
-			files: ['lib/*.js', 'spec/*.js']
+			files: ['lib/*.js', 'spec/*.js', 'Gruntfile.js']
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 
-}
+};
--- spec/converter-spec.js
+++ spec/converter-spec.js
@@ -1,12 +1,67 @@
 var Converter = require('../lib/converter');
+var fs = require('fs');
 
-describe('コンバーター', function() {
-	it("呼び出せること", function() {
+var infile = 'sample.csv',
+	outfile = 'sample.json',
+	csvPath = 'spec/fixtures/' + infile,
+	jsonPath = 'spec/' + outfile;
+
+describe('Converter', function() {
+
+	beforeEach(function() {
+		cleanTestFiles([jsonPath]);
+	});
+
+	afterEach(function() {
+		cleanTestFiles([jsonPath]);
+	});
+
+	it("shouhld be called.", function() {
 		expect(Converter).toBeDefined();
 	});
 
-	it("jsonを出力すること", function() {
-		Converter.csv2json();
-		expect(true).toEqual(true);
+	it("outputs json file", function() {
+
+		Converter.csv2json(csvPath, jsonPath, function(row, index) {
+			return {
+				"cd"		: row[0],
+				"rr_cd"		: row[1],
+				"name"		: row[2],
+				"name_k"	: row[3],
+				"name_h"	: row[4],
+				"url"		: row[6],
+				"status"	: [row[7], row[8], row[9]]
+			};
+		});
+		waits(100);
+
+		runs(function() {
+			expect(true).toEqual(fs.existsSync(jsonPath));
+
+			var companies = require('./' + outfile);
+			var company = companies[0];
+			expect(165).toEqual(companies.length);
+			expect('1').toEqual(company.cd);
+			expect('11').toEqual(company.rr_cd);
+			expect('JR北海道').toEqual(company.name);
+			expect('ジェイアールホッカイドウ').toEqual(company.name_k);
+			expect('北海道旅客鉄道株式会社').toEqual(company.name_h);
+			expect('http://www.jrhokkaido.co.jp/').toEqual(company.url);
+			expect(['1','0','1']).toEqual(company.status);
+		});
+
+
 	});
 });
+
+
+// Utility
+function cleanTestFiles(files)
+{
+	for (var i in files) {
+		var path = files[i];
+		if (fs.existsSync(path)) {
+			fs.unlinkSync(path);
+		}
+	}
+}
