4cc9d557dd005e9ff0d90da852974c347f9e6d12
Masato OSHIMA <qqw75pn9@gmail.com>
2014-03-09 19:34:54 +0900

    Added jsdoc3 document

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,14 +3,23 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 		watch: {
 			files: ['lib/*.js', 'spec/*.js', 'Gruntfile.js'],
-			tasks: ['jshint']
+			tasks: ['jshint', 'shell:jsdoc']
 		},
 		jshint: {
 			files: ['lib/*.js', 'spec/*.js', 'Gruntfile.js']
+		},
+		shell: {
+			jsdoc: {
+				options: {
+					stdout: true
+				},
+				command: 'node_modules/.bin/jsdoc lib/converter.js -d docs'
+			}
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-shell');
 
 };
