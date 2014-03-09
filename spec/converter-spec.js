4233ec55be72e3c4b196ef47c0203b332b31fb83
Masato OSHIMA <qqw75pn9@gmail.com>
2014-03-10 00:43:39 +0900

    Implemented 2 json join via forien key

--- spec/converter-spec.js
+++ spec/converter-spec.js
@@ -1,19 +1,18 @@
-var Converter = require('../lib/converter');
-var fs = require('fs');
+describe('Converter csv2json', function() {
+	var Converter = require('../lib/converter');
+	var fs = require('fs');
 
-var infile = 'company.csv',
-	outfile = 'company.json',
-	csvPath = 'spec/fixtures/' + infile,
-	jsonPath = 'spec/' + outfile;
-
-describe('Converter', function() {
+	var aCsvFile = 'company.csv', aJsonFile = 'company.json',
+		bCsvFile = 'line.csv',    bJsonFile = 'line.json',
+		aCsvPath = 'spec/fixtures/' + aCsvFile, aJsonPath = 'spec/' + aJsonFile,
+		bCsvPath = 'spec/fixtures/' + bCsvFile, bJsonPath = 'spec/' + bJsonFile;
 
 	beforeEach(function() {
-		cleanTestFiles([jsonPath]);
+		cleanTestFiles([aJsonPath, bJsonPath]);
 	});
 
 	afterEach(function() {
-		cleanTestFiles([jsonPath]);
+		cleanTestFiles([aJsonPath, bJsonPath]);
 	});
 
 	it("shouhld be called.", function() {
@@ -22,35 +21,79 @@ describe('Converter', function() {
 
 	it("outputs json file", function() {
 
-		Converter.csv2json(csvPath, jsonPath, function(row, index) {
-			return {
-				"cd"		: row[0],
-				"rr_cd"		: row[1],
-				"name"		: row[2],
-				"name_k"	: row[3],
-				"name_h"	: row[4],
-				"url"		: row[6],
-				"status"	: [row[7], row[8], row[9]]
-			};
+		runs(function() {
+			Converter.csv2json(aCsvPath, aJsonPath, function(row, index) {
+				return {
+					"cd"		: row[0],
+					"rr_cd"		: row[1],
+					"name"		: row[2],
+					"name_k"	: row[3]
+				};
+			});
 		});
+		
 		waits(100);
 
 		runs(function() {
-			expect(true).toEqual(fs.existsSync(jsonPath));
+			expect(true).toEqual(fs.existsSync(aJsonPath));
 
-			var companies = require('./' + outfile);
+			var companies = require('./' + aJsonFile);
 			var company = companies[0];
-			expect(165).toEqual(companies.length);
+			expect(6).toEqual(companies.length);
 			expect('1').toEqual(company.cd);
 			expect('11').toEqual(company.rr_cd);
 			expect('JR北海道').toEqual(company.name);
 			expect('ジェイアールホッカイドウ').toEqual(company.name_k);
-			expect('北海道旅客鉄道株式会社').toEqual(company.name_h);
-			expect('http://www.jrhokkaido.co.jp/').toEqual(company.url);
-			expect(['1','0','1']).toEqual(company.status);
 		});
+	});
 
+	it("shouhld join", function() {
 
+		runs(function() {
+			Converter.csv2json(aCsvPath, aJsonPath, function(row, index) {
+				return {
+					"cd"	: row[0],
+					"name"	: row[2],
+					"lines"	: row[10].split(',')
+				};
+			});
+		});
+		runs(function() {
+			Converter.csv2json(bCsvPath, bJsonPath, function(row, index) {
+				return {
+					"cd"	: row[0],
+					"name"	: row[2],
+					"lan"	: row[8],
+					"lot"	: row[9]
+				};
+			});
+		});
+		waits(100);
+
+		runs(function() {
+			expect(fs.existsSync(aJsonPath)).toBeTruthy();
+			expect(fs.existsSync(bJsonPath)).toBeTruthy();
+
+			var companies = JSON.parse(fs.readFileSync(aJsonPath, 'utf8')),
+				lines     = JSON.parse(fs.readFileSync(bJsonPath, 'utf8'));
+
+			expect(3).toEqual(companies[0].lines.length);
+			expect('11101').toEqual(lines[0].cd);
+			expect('11102').toEqual(lines[1].cd);
+			expect('11103').toEqual(lines[2].cd);
+
+			companies = Converter.joinJson(companies, lines);
+
+			expect(3).toEqual(companies[0].lines.length);
+			expect('11101').toEqual(companies[0].lines[0].cd);
+			expect('JR函館本線(函館〜長万部)').toEqual(companies[0].lines[0].name);
+			expect('140.4833696755387').toEqual(companies[0].lines[0].lan);
+			expect('42.23981411231523').toEqual(companies[0].lines[0].lot);
+
+			expect('11102').toEqual(companies[0].lines[1].cd);
+			expect('11103').toEqual(companies[0].lines[2].cd);
+
+		});
 	});
 });
 
@@ -58,6 +101,8 @@ describe('Converter', function() {
 // Utility
 function cleanTestFiles(files)
 {
+	var fs = require('fs');
+
 	for (var i in files) {
 		var path = files[i];
 		if (fs.existsSync(path)) {
